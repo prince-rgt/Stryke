554b3e1af5aacb3da5195ea8f243de88f8f4d41f
Shivansh070707 <shivansh0301@gmail.com>
2025-03-07 15:14:41 +0530

    feat: export wallet configuration and add contract constants

---
+++ src/consts/contracts.ts
@@ -0,0 +1,28 @@
+const contracts = {
+  strykeVault: {
+    42161: {
+      usdc: '',
+      wETH: '',
+      wBTC: '',
+    },
+    421614: {
+      usdc: '0xFDa0543dA03FA442381135c69796496155Eb05Fc',
+      wETH: '',
+      wBTC: '',
+    },
+  },
+  usdc: {
+    42161: '',
+    421614: '',
+  },
+  wETH: {
+    42161: '',
+    421614: '',
+  },
+  wBTC: {
+    42161: '',
+    421614: '',
+  },
+};
+
+export default contracts;
